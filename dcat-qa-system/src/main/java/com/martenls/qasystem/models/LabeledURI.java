4b90c4fe547ef82574c859281ec5e5594f3bc6d9
Marten Schmidt <marten.l.schmidt@gmail.com>
2020-06-25 17:00:43 +0200

    update ontology parsing + reenable test ci job

--- dcat-qa-system/src/main/java/com/martenls/qasystem/models/LabeledURI.java
+++ dcat-qa-system/src/main/java/com/martenls/qasystem/models/LabeledURI.java
@@ -1,6 +1,5 @@
 package com.martenls.qasystem.models;
 
-import com.fasterxml.jackson.annotation.JsonAnySetter;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
@@ -19,16 +18,17 @@ public class LabeledURI {
     private String uri;
     private Map<String, List<String>> labels;
 
-    @JsonCreator
-    public LabeledURI(@NonNull @JsonProperty("uri") String uri) {
+    public LabeledURI(@NonNull String uri) {
         this.uri = uri;
         this.labels = new HashMap<>();
     }
 
-    @JsonAnySetter
-    public void setLabels(String name, List<String> value) {
-        this.labels.put(name, value);
+    @JsonCreator
+    public LabeledURI(@NonNull @JsonProperty("uri") String uri, @JsonProperty("labels") Map<String, List<String>> labels) {
+        this.uri = uri;
+        this.labels = labels;
     }
+
 
 }
 
