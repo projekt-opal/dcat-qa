f0159d6deeac329d877bfd6efdae27dc950e282c
Marten Schmidt <martenls@mail.upb.de>
2020-08-27 10:27:11 +0200

    qa: fix de nlp pipeline, web-bot: fix opal logo

--- dcat-qa-system/src/main/java/com/martenls/qasystem/config/StanfordNLPConfig.java
+++ dcat-qa-system/src/main/java/com/martenls/qasystem/config/StanfordNLPConfig.java
@@ -5,9 +5,8 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.util.ResourceUtils;
+import org.springframework.core.io.ClassPathResource;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
@@ -31,7 +30,7 @@ public class StanfordNLPConfig {
     @Bean
     public StanfordCoreNLP getDePipeline() {
         Properties properties = new Properties();
-        try (InputStream input = new FileInputStream(ResourceUtils.getFile("classpath:StanfordCoreNLP-german.properties"))) {
+        try (InputStream input = new ClassPathResource("StanfordCoreNLP-german.properties").getInputStream()) {
             // load a properties file
             properties.load(input);
         } catch (IOException e) {
